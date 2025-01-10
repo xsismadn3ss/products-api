b9ebb002944b2f56dae14583b37255953e622d85
xsismadn3ss <2477carlos.artigaq@gmail.com>
2025-01-10 16:10:22 +0000

    configurar server

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import express from 'express';
-const app = express();
+import app from "./app";
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
-});
-
-const port = parseInt(process.env.PORT) || 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+app.listen(
+  app.get('port'), () =>{
+    console.log('Servidor iniciado ✅')
+  }
+)
