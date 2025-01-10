3e8c9454e8f0eed38fecdc162ab122e6edd41cb3
xsismadn3ss <2477carlos.artigaq@gmail.com>
2025-01-10 16:11:28 +0000

    crear modelo producto

---
+++ models/producto.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Producto extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Producto.init({
+    nombre: {type: DataTypes.STRING, allowNull: false},
+    descripcion: {type: DataTypes.STRING, allowNull:false},
+    precio: {type: DataTypes.DECIMAL(10,2), allowNull:false, },
+    idCategoria: {type: DataTypes.INTEGER, allowNull:false},
+    activo: {type: DataTypes.BOOLEAN, defaultValue:true}
+  }, {
+    sequelize,
+    modelName: 'Producto',
+  });
+  return Producto;
+};
